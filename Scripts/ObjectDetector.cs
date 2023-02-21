2b09417c7b612c6c53bd5922ef74adb3827e28d2
Dr Kevin Gary <ser516asu@gmail.com>
2023-02-20 18:13:02 -0700

    Task-33-Obj detection YOLO Model Configuration

--- Scripts/ObjectDetector.cs
+++ Scripts/ObjectDetector.cs
@@ -15,4 +15,49 @@ public class ObjectDetector : MonoBehaviour
         session = new TFSession(graph);
         runner = session.GetRunner();
     }
+
+    void Update()
+    {
+        // Capture a screenshot of the area where the objects will be detected
+        Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture();
+
+        // Convert the screenshot to a tensor
+        var tensor = TransformInput(texture);
+
+        // Perform object detection
+        runner.AddInput(graph["input_1"][0], tensor);
+        runner.Fetch(
+            graph["yolo_nms"][0],
+            graph["yolo_nms_1"][0],
+            graph["yolo_nms_2"][0]
+        );
+        var output = runner.Run();
+
+        // Display the results of object detection
+        var boxes = output[0].GetValue() as float[,,];
+        var classes = output[1].GetValue() as float[,,];
+        var scores = output[2].GetValue() as float[,,];
+        DisplayResults(boxes, classes, scores);
+    }
+
+    private TFTensor TransformInput(Texture2D texture)
+    {
+        var colors = texture.GetPixels32();
+        var channels = new float[colors.Length * 3];
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            channels[i] = colors[i].r;
+            channels[i + colors.Length] = colors[i].g;
+            channels[i + colors.Length * 2] = colors[i].b;
+        }
+
+        return TFTensor.FromBuffer(new long[] { 1, texture.height, texture.width, 3 }, channels, 0, channels.Length);
+    }
+
+    private void DisplayResults(float[,,] boxes, float[,,] classes, float[,,] scores)
+    {
+        // TODO
+    }
 }
+
