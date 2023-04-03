30c0da53dcfac2365b0839cf60ad482fdf541067
jashmin patel <jasmin.3998@gmail.com>
2023-04-02 23:15:00 -0700

    task-63 add script to enable-disable camera flash

---
+++ ObjectDetection/Scripts/FlashlightController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FlashlightController : MonoBehaviour {
+
+    private bool isFlashOn = false;
+    private Camera mainCamera;
+
+    void Start () {
+        mainCamera = Camera.main;
+        if (!Application.HasUserAuthorization(UserAuthorization.WebCam)) {
+            Debug.Log("No camera permission granted.");
+            return;
+        }
+    }
+
+    public void ToggleFlashlight () {
+        if (!isFlashOn) {
+            mainCamera.GetComponent<Light>().enabled = true;
+            isFlashOn = true;
+        } else {
+            mainCamera.GetComponent<Light>().enabled = false;
+            isFlashOn = false;
+        }
+    }
+}
