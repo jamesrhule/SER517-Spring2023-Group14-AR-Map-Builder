62eda8e51a36fff35305b596373f9cf9ff1de030
pmehta32 <119138980+pmehta32@users.noreply.github.com>
2023-04-21 23:24:45 -0700

    Edited the AR model
    When the status of the trackable changes, the OnTrackableStateChanged method is called, which determines whether the target is detected, tracked, or lost.
    
    Taiga Link: https://tree.taiga.io/project/jmpatel3998-ser517-ar-map-builder/task/78

---
+++ ARModelEdit.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ARModelController : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+
+    private void OnTrackingFound()
+    {
+        // Show the model when the target is detected and tracked
+        gameObject.SetActive(true);
+    }
+
+    private void OnTrackingLost()
+    {
+        // Hide the model when the target is lost
+        gameObject.SetActive(false);
+    }
+}
